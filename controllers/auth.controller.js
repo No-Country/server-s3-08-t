ac909da09445129b11c5f0bde6c30c369f45c7a8
ing. Jaime Agudelo <ingenieroagudelo@gmail.com>
2022-08-09 08:52:31 -0500

    Actualización de auth.controler 😁

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -2,36 +2,36 @@ const { request, response } = require("express");
 const Users = require("../models/users");
 
 const authController = async (req = request, res = response) => {
-    const { userName, password } = req.body;
+  const { userName, password } = req.body;
 
-    try {
-        const user = await Users.findOne({
-            userName
-        });
-        if(!user) {
-            return res.status(400).json({
-                msg: 'Usuario no encontrado'
-            })
-        };
-        const passw = await Users.findOne({
-            password
-        });
-        if(passw != password) {
-            return res.status(400).json({
-                msg: 'Contraseña incorrecta'
-            })
-        };
-        res.status(200).json({
-            msg: 'Succes',
-            userName, password
-        })
-        
-    } catch (error) {
-        console.log(error)
-        res.status(500).json({
-            msg: 'Error del servidor'
-        })
+  try {
+    const user = await Users.findOne({
+      userName,
+    });
+    console.log(user);
+    if (!user) {
+      return res.status(400).json({
+        msg: "Usuario no encontrado",
+      });
     }
-}
+    // const passw = await Users.findOne({
+    //   password,
+    // });
+    if (password != user.password) {
+      return res.status(400).json({
+        msg: "Contraseña incorrecta",
+      });
+    }
+    res.status(200).json({
+      msg: "Succes",
+      user,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      msg: "Error del servidor",
+    });
+  }
+};
 
 module.exports = authController;
